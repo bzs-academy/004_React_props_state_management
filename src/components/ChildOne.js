4174000f780c7775fd80e4566bb0ab15eb60d1ae
bzs-academy <education@bzsacademy.com>
2020-03-04 21:18:19 +0100

    first commit

---
+++ src/components/ChildOne.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ChildTwo from './ChildTwo';
+
+
+function ChildOne ({ isim, sehir }) {
+
+    // const { isim } = props;
+
+    return (
+        <div>
+            <p>Hosgeldiniz , {isim} </p>
+            <ChildTwo sehir= {sehir} />
+        </div>
+    )
+
+
+}
+
+export default ChildOne;
